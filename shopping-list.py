1c7f6178c28c18ba15ace0b3383625421b34981a
Tim Holmes <tholmes@starchase.com>
2016-02-11 11:03:38 -0500

    Split calls up into functions.

--- shopping-list.py
+++ shopping-list.py
@@ -7,27 +7,44 @@
 def main():
     shopping_list = []
 
-    start_message = 'What should we pick up at the store?\nPrint SEE to view the list\nPrint DONE to stop adding items.'
-
-    print(start_message)
+    print('### Welcome to Python Shopping List ### \n\nWhat should we pick up at the store?')
+    show_help()
 
     while True:
-        new_item = input().lower()
+        new_item = input('> ').lower()
 
         if new_item == 'done':
-            print('\nFinal list:')
-            print_list(shopping_list)
-            print(new_item)
+            quit(shopping_list)
             break
-        elif new_item == 'see':
-            print('\nCurrent list:')
-            print_list(shopping_list)
-            print('\nKeep adding items:')
-        else:
-            shopping_list.append(new_item)
+        elif new_item == 'show':
+            show_list(shopping_list)
+            continue
+        elif new_item == 'help':
+            show_help()
+            continue
 
-def print_list(print_this):
-    for x in print_this:
+        shopping_list = add_new_item(new_item, shopping_list)
+
+def print_list(list_x):
+    for x in list_x:
         print(x)
+
+def show_list(list_x):
+    print('\nCurrent list:')
+    print_list(list_x)
+    print('\nKeep adding items:')
+
+def quit(list_x):
+    print('\nFinal list:')
+    print_list(list_x)
+    print('\nGoodbye!')
+
+def show_help():
+    print('\nSpecial commands:\nDONE : quit the app\nSHOW : view the list\nHELP : help menu\n')
+
+def add_new_item(x, list_x):
+    list_x.append(x)
+    print('Added {}. List has {} items.'.format(x, len(list_x)))
+    return list_x
 
 main()
